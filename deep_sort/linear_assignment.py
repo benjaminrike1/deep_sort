4a88161907635fa23bdbaa63772115e842aaba3e
benjaminrike1 <benjaminrike1@gmail.com>
2022-04-21 20:51:03 +0200

    reverted to sklearn linear_assignment

--- deep_sort/linear_assignment.py
+++ deep_sort/linear_assignment.py
@@ -1,9 +1,7 @@
 # vim: expandtab:ts=4:sw=4
 from __future__ import absolute_import
 import numpy as np
-from scipy.optimize import linear_sum_assignment as linear_assignment
-from . import kalman_filter
-
+from sklearn.utils.linear_assignment_ import linear_assignment
 
 INFTY_COST = 1e+5
 
